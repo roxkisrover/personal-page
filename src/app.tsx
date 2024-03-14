f7d44d274b04772c5705272849a95eb968bd3590
Anton Nikolaenko <roxkisrover@gmail.com>
2024-03-14 15:22:40 +0500

    feat: add animation

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,35 @@
+import { useSpring, animated } from "@react-spring/web";
+
 import styles from "./App.module.scss";
 import logo from "./assets/images/x5-tech-logo.svg";
 
 function App() {
+  const logoSprings = useSpring({
+    from: { y: -100, opacity: 0 },
+    to: { y: -15, opacity: 1 },
+    delay: 200,
+  });
+  const nameSprings = useSpring({
+    from: { y: 100, opacity: 0 },
+    to: { y: 0, opacity: 1 },
+    delay: 200,
+  });
+
   return (
     <div className={styles.container}>
       <div className={styles.hero}>
         <div className={styles.title}>
-          <img className={styles.logo} src={logo} alt={"X5 Tech"} />
+          <animated.img
+            className={styles.logo}
+            src={logo}
+            alt={"X5 Tech"}
+            draggable={false}
+            style={logoSprings}
+          />
           <div className={styles.times}>&times;</div>
-          <h1 className={styles.name}>Anton Nikolaenko</h1>
+          <animated.h1 className={styles.name} style={nameSprings}>
+            Anton Nikolaenko
+          </animated.h1>
         </div>
       </div>
     </div>
